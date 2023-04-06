bdb974e0e5438ff6a050b14ea5064d6ad8d562f2
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-04-06 17:31:06 -0300

    adding category repository to product services

--- src/domain/services/product-services/ProductServices.ts
+++ src/domain/services/product-services/ProductServices.ts
@@ -1,13 +1,17 @@
+import ICategoryRepository from '../../repositories/ICategoryRepository'
 import IProductRepository from '../../repositories/IProductRepository'
 
 export type productServicesDepedencies = {
   productRepository: IProductRepository
+  categoryRepository: ICategoryRepository
 }
 
 export default abstract class ProductServices {
   protected readonly productRepository: IProductRepository
-  constructor ({ productRepository }: productServicesDepedencies) {
+  protected readonly categoryRepository: ICategoryRepository
+  constructor ({ productRepository, categoryRepository }: productServicesDepedencies) {
     this.productRepository = productRepository
+    this.categoryRepository = categoryRepository
   }
 
   abstract execute(...args: unknown[]): Promise<unknown>
