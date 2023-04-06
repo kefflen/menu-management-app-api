2456ca40de6c911505987bc9dc6b11074fe14f7b
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-04-06 13:48:41 -0300

    creating entity User

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,20 @@
+
+export type userDTO = {
+  id?: string
+  name: string
+  password: string
+  isAdmin: boolean
+}
+
+export default class User {
+  public readonly id?: string
+  public name: string
+  public password: string
+  public isAdmin: boolean
+  constructor({id, name, password, isAdmin}: userDTO) {
+    this.id = id
+    this.name = name
+    this.password = password
+    this.isAdmin = isAdmin
+  }
+}
