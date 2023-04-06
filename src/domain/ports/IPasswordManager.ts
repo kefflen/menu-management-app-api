921525d248927f0c931faeb02150ab732e5cb5a6
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-04-06 14:09:44 -0300

    creating interface IPasswordManager

---
+++ src/domain/ports/IPasswordManager.ts
@@ -0,0 +1,5 @@
+
+export default interface IPasswordManager {
+  encryptPassword(password: string): Promise<string>
+  verifyPassword(descriptedPassword: string, encryptedPassword: string): Promise<boolean>
+}
