6b2c0645e0c72bea54e4b8464c3522e3ee15ca37
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-04-06 16:49:55 -0300

    changing call of update user so receive first the id and receive partial of update properties

--- src/domain/services/user-services/UpdateUserService.ts
+++ src/domain/services/user-services/UpdateUserService.ts
@@ -2,14 +2,14 @@ import User, { userDTO } from '../../entities/User'
 import AppError from '../../errors/AppError'
 import { UserServices } from './UserServices'
 
-type updateUserParam = Pick<userDTO, 'id' | 'name'>
+type updateUserParam = Partial<Pick<userDTO, 'name'>>
 export default class UpdateUserService extends UserServices {
-  async execute({ id, name }: updateUserParam): Promise<User> {
-    const user = await this.userRepository.getById(id)
+  async execute(userId: string, { name }: updateUserParam): Promise<User> {
+    const user = await this.userRepository.getById(userId)
     if (user === null) throw AppError.notFound('User not found')
-    user.name = name
+    if (name) user.name = name
     const updatedUser = await this.userRepository.update(user)
-    
+
     return updatedUser
   }
 }
