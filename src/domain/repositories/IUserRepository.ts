3e6db4fb3c6471b935eac1ab703c4a9e594127b9
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-04-07 15:48:05 -0300

    add method to user repository interface to find by email

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -1,4 +1,6 @@
 import User from '../entities/User'
 import IEntityRepository from './IEntityRepository'
 
-export default interface IUserRepository extends IEntityRepository<User> {}
+export default interface IUserRepository extends IEntityRepository<User> {
+  getUserByEmail(email: string): Promise<User|null>
+}
