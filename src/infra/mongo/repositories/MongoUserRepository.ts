0f55f0b872f630f1e8555778206601025960057b
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-04-06 22:54:08 -0300

    implementing user repository with mongo

---
+++ src/infra/mongo/repositories/MongoUserRepository.ts
@@ -0,0 +1,35 @@
+import User from '../../../domain/entities/User'
+import AppError from '../../../domain/errors/AppError'
+import IUserRepository from '../../../domain/repositories/IUserRepository'
+import UserModel from '../models/UserModel'
+
+export default class MongoUserRepository implements IUserRepository {
+  async create(user: User): Promise<User> {
+    const userDTO = await UserModel.create(user)
+    return new User(userDTO)
+  }
+
+  async list(): Promise<User[]> {
+    const usersDTO = await UserModel.find()
+    return usersDTO.map(userDTO => new User(userDTO))
+  }
+
+  async getById(userId: string): Promise<User | null> {
+    const userDTO = await UserModel.findById(userId)
+    if (!userDTO) return null
+    return new User(userDTO)
+  }
+
+  async update(user: User): Promise<User> {
+    const userDTO = await UserModel.findOneAndUpdate(user)
+
+    if (!userDTO) throw AppError.notFound('User not found')
+
+    return new User(userDTO)
+  }
+
+  async delete(userId: string): Promise<void> {
+    await UserModel.deleteOne({ _id: userId })
+  }
+
+}
