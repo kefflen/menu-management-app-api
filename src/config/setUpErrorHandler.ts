7b1bb90fd78587f8a86903788c8949dc55168035
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-04-07 22:13:04 -0300

    installing zod and setting the error handle to handle zod error

--- src/config/setUpErrorHandler.ts
+++ src/config/setUpErrorHandler.ts
@@ -1,4 +1,5 @@
 import { Express, Request, Response, NextFunction } from 'express'
+import { ZodError } from 'zod'
 import AppError from '../domain/errors/AppError'
 
 export default function setUpErrorHandler(express: Express) {
@@ -10,15 +11,21 @@ export default function setUpErrorHandler(express: Express) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       nextFunction: NextFunction
     ) => {
-      console.log(error)
 
       if (error instanceof AppError) {
+        console.log(error)
         return response.status(error.code).json({
-          message: error.message,
+          error: error.message,
+        })
+      } else if (error instanceof ZodError) {
+        console.log(error.errors)
+        return response.status(400).json({
+          errors: error.errors.map(error => error.message)
         })
       } else {
+        console.log(error)
         return response.status(500).json({
-          message: 'Internal Server Error',
+          error: 'Internal Server Error',
         })
       }
     }
